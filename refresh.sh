0e3eddfe01c32dfa42a3a440a66374cd77842adb
geo-petrini <geo.petrini@gmail.com>
2022-10-10 22:18:41 +0200

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-#diffpull=!git fetch && git diff HEAD..@{u}
+echo Check delta between local and remote repo
+
+#echo listing differences and calculating word count for the if condition
+DIFFPULL=$(git fetch && git diff HEAD..@{u} | wc -m)
+
+if [ "$DIFFPULL" != "0" ]; then
+	echo There are updates in the remote repo -> executing pull
+	#executing a force pull disregarding local changes
+	git fetch
+	git reset --hard HEAD
+	git merge
+fi
+
+echo end of script
