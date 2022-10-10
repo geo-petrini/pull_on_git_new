413055b83368728ba7c8e58915d936b6303ce596
geo-petrini <geo.petrini@gmail.com>
2022-10-10 22:29:53 +0200

    edited readme and script output

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# pull_on_git_new
-Pull content of git repository on new changes in remote repo
+# Pull On Git New
+Bash script to pull content of git repository on new changes in remote repo.
+This script forces a pull disregarding any local changes.
+
+## Usage
+Set run permissions `chmod +x refresh.sh` and run `refresh.sh` or execute `bash refresh.sh`
+
+Customize at will
+
+
+## TODO
+- Windows version (cmd or ps)
+- Arguments such as other scripts to run before or after pull
+- Logging
+- Better error handling
--- refresh.sh
+++ refresh.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-echo Check delta between local and remote repo
-
 #echo listing differences and calculating word count for the if condition
 DIFFPULL=$(git fetch && git diff HEAD..@{u} | wc -m)
 
 if [ "$DIFFPULL" != "0" ]; then
-	echo There are updates in the remote repo -> executing pull
+	echo There are updates in the remote repo, running forced pull
 	#executing a force pull disregarding local changes
 	git fetch
 	git reset --hard HEAD
 	git merge
+else
+	echo Nothing to update
 fi
-
-echo end of script
